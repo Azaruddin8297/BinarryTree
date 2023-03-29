d7dfa10e6d14c0b227df30d19d5f5c6abf570d92
Azaruddin8297 <ajarpopo.com>
2023-03-29 23:41:24 +0530

    FirstCase

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class BinaryTree
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                {
+                    after = after.leftNode;
+                }
+                else if (value > after.Data)
+                {
+                    after = after.rightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                {
+                    before.leftNode = newNode;
+                }
+                else
+                {
+                    before.rightNode = newNode;
+                }
+               
+            }
+            Console.WriteLine("{0} is added in List", newNode.Data);
+
+            return true;
+        }
+        public void TraversePreorder(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.WriteLine(parent.Data + " ");
+                TraversePreorder(parent.leftNode);
+                TraversePreorder(parent.rightNode);
+            }
+        }
+    }
+}
---
+++ BinaryTree/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Node
+    {
+        public int Data { get; set; }
+        public Node leftNode { get; set; }
+        public Node rightNode { get; set; }
+    }
+}
